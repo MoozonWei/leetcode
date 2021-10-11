c579e28b6c25ad25ddf60ec04d1f32db132467db
魏子涵 <muzhenweizh@gmail.com>
2021-10-11 11:58:41 +0800

    dp

---
+++ 64.最小路径和.js
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=64 lang=javascript
+ *
+ * [64] 最小路径和
+ *
+ * https://leetcode-cn.com/problems/minimum-path-sum/description/
+ *
+ * algorithms
+ * Medium (68.83%)
+ * Likes:    1025
+ * Dislikes: 0
+ * Total Accepted:    272.6K
+ * Total Submissions: 396.1K
+ * Testcase Example:  '[[1,3,1],[1,5,1],[4,2,1]]'
+ *
+ * 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ * 
+ * 说明：每次只能向下或者向右移动一步。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+ * 输出：7
+ * 解释：因为路径 1→3→1→1→1 的总和最小。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：grid = [[1,2,3],[4,5,6]]
+ * 输出：12
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * m == grid.length
+ * n == grid[i].length
+ * 1 
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+    const height = grid.length
+    const width = grid[0].length
+
+    const dp = new Array(height).fill(0).map(() => new Array(width).fill(0))
+    dp[0][0] = grid[0][0]
+
+    // top line initialization
+    for (let i = 1; i < width; i++) {
+        dp[0][i] = dp[0][i-1] + grid[0][i]
+    }
+    // left colum initialization
+    for (let i = 1; i < height; i++) {
+        dp[i][0] = dp[i-1][0] + grid[i][0]
+    }
+    // other dps
+    for (let i = 1; i < height; i++) {
+        for (let j = 1; j < width; j++) {
+            dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+        }
+    }
+
+    return dp[height-1][width-1]
+};
+// @lc code=end
+
